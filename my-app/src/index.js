ee4116ba9b5f500f93febb2d88f92414a53346d7
Alexandr <scheitanik@gmail.com>
2021-06-27 12:14:39 +0300

    Firt Commit

---
+++ my-app/src/index.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// import App from './components/app';
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 26,
+      nationality: "by"
+    }
+
+    // this.nextYear = () => {
+    //   this.setState(state => ({
+    //     years: ++state.years
+    //   }))
+    // }
+    // this.nextYear = this.nextYear.bind(this);
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+      years: ++state.years
+    }))
+  }
+
+  // nextYear() {
+  //   console.log(1);
+  //   this.setState(state => ({
+  //     years: ++state.years
+  //   }))
+  // }
+  render() {
+    const {name, surname, link} = this.props;
+    return (
+      <>
+        <button onClick={this.nextYear}>++</button>
+        <h1>My name is {name}, surname - {surname}, years {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </>
+    )
+  }
+}
+
+
+const All =() => {
+  return (
+    <>
+      <WhoAmI name="John" surname="Smith" link="facebook.com"/>
+      <WhoAmI name="Ivan" surname="Smith" link="vk.com"/>
+      <WhoAmI name="Jhon" surname="Shepard" link="facebook.com"/>
+    </>
+  )
+}
+
+
+
+
+ReactDOM.render(
+  <All/>,
+  document.getElementById('root')
+);
+
