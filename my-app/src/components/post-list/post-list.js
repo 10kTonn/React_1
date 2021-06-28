821e4672cd33790b11429f447c9af1ae77894615
Alexandr <scheitanik@gmail.com>
2021-06-28 14:57:15 +0300

    Added styled components

--- my-app/src/components/post-list/post-list.js
+++ my-app/src/components/post-list/post-list.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PostListItem from '../post-list-item';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 import './post-list.css'
 
 const PostList = ({posts}) => {
@@ -26,9 +27,9 @@ const PostList = ({posts}) => {
     }
 
     return (
-        <ul className="app-list list-group">
+        <ListGroup className="app-list list-group">
            {elements}
-        </ul>
+        </ListGroup>
     )
 }
 
