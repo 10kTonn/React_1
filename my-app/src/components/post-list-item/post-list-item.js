c240b12e0ba1c7d34e5cf2863cba16f6b6b2b9ad
Alexandr <scheitanik@gmail.com>
2021-06-29 10:34:36 +0300

    Fixed function onLike in new branch style

--- my-app/src/components/post-list-item/post-list-item.js
+++ my-app/src/components/post-list-item/post-list-item.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import './post-list-item.css';
+import './post-list-item.css'
 
 export default class PostListItem extends Component {
     constructor(props) {
@@ -29,38 +29,38 @@ export default class PostListItem extends Component {
         const {label, onDelete} = this.props;
         const {important, like} = this.state;
         let classNames = 'app-list-item d-flex justify-content-between';
-         if (important) {
-        classNames += ' important';
+
+        if (important) {
+            classNames +=' important';
         }
 
-        if(like) {
-            classNames += ' like';
+        if (like) {
+            classNames +=' like';
         }
 
         return (
-                <div className={classNames}>
-                <span className="app-list-item-label"
-                      onClick={this.onLike}>
+            <div className={classNames}>
+                <span 
+                className="app-list-item-label"
+                onClick={this.onLike}>
                     {label}
                 </span>
                 <div className="d-flex justify-content-center align-items-center">
                     <button 
-                        className="btn-star btn-sm" 
-                        type="button"
-                        onClick={this.onImportant}>
-                        <i className="bi bi-star"></i>
+                    type="button" 
+                    className="btn-star btn-sm"
+                    onClick={this.onImportant}>
+                        <i className="fa fa-star"></i>
                     </button>
                     <button 
-                        className="btn-trash btn-sm" 
-                        type="button"
+                        type="button" 
+                        className="btn-trash btn-sm"
                         onClick={onDelete}>
-                        <i className="bi bi-trash"></i>
+                        <i className="fa fa-trash-o"></i>
                     </button>
-                    <i className="bi bi-suit-heart"></i>
+                    <i className="fa fa-heart"></i>
                 </div>
             </div>
         )
-    }
+        }
 }
-
-
